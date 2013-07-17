e5d99cb0655a3014275fd7f739e21b7dfe8ceb3a
Kael <i@kael.me>
2013-07-17 21:06:12 +0800

    add sample user script

---
+++ sample/sample.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @match http://*/*
+// @match http://*.google.com/*
+// @match http://www.google.com/*
+// ==/UserScript==
+
+// 上面的注解，不要在前面或者中间添加奇怪的东西
+
+// 打开 chrome 浏览器的 chrome://extensions 页面，将本脚本拖拽到页面空白处，并确认。
+
+console.log('hello world!');
+console.log('My userAgent is:', navigator.userAgent);
